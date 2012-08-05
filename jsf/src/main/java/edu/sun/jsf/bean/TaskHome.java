6a41d55491907cccc30dd6ab14b2d020e613a85c
qipengsun <qipeng.sun@gmail.com>
2012-08-06 00:38:26 +0800

    format

--- jsf/src/main/java/edu/sun/jsf/bean/TaskHome.java
+++ jsf/src/main/java/edu/sun/jsf/bean/TaskHome.java
@@ -1,49 +1,49 @@
 package edu.sun.jsf.bean;
 
-import java.util.List;
-
 import edu.sun.jsf.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component("taskHome")
 public class TaskHome {
 
-	private static final Logger logger = LoggerFactory.getLogger(TaskHome.class);
+    private static final Logger logger = LoggerFactory.getLogger(TaskHome.class);
 
-	private Task task = new Task();
-	private List<Task> tasks;
+    private Task task = new Task();
+    private List<Task> tasks;
 
-	@Autowired
-	private TaskDao taskDao;
+    @Autowired
+    private TaskDao taskDao;
 
-	
-	public String getMessage() {
-		logger.debug("Returning message from task home bean");
-		return "Hello from Spring";
-	}	
 
-	public Task getTask() {
-		return task;
-	}
+    public String getMessage() {
+        logger.debug("Returning message from task home bean");
+        return "Hello from Spring";
+    }
 
-	public void saveTask() {
-		taskDao.save(task);
-		task = new Task();
-		invalidateTasks();
-	}
+    public Task getTask() {
+        return task;
+    }
 
-	private void invalidateTasks() {
-		tasks = null;
-	}
+    public void saveTask() {
+        taskDao.save(task);
+        task = new Task();
+        invalidateTasks();
+    }
 
-	public List<Task> getTasks() {
-		if (tasks == null) {
-			tasks = taskDao.list();
-		}
-		return tasks;
-		
-	}
+    private void invalidateTasks() {
+        tasks = null;
+    }
+
+    public List<Task> getTasks() {
+        if (tasks == null) {
+            tasks = taskDao.list();
+        }
+        return tasks;
+
+    }
 }
--- jsf/src/test/java/edu/sun/jsf/bean/TaskHomeTest.java
+++ jsf/src/test/java/edu/sun/jsf/bean/TaskHomeTest.java
@@ -2,7 +2,6 @@ package edu.sun.jsf.bean;
 
 import edu.sun.jsf.model.Task;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,24 +13,24 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class TaskHomeTest {
-	
-	@Autowired
-	private TaskHome taskHome;
-		
-	@Test
-	public void shouldGetCorrectMessage() {			
-		String message = taskHome.getMessage();
-		Assert.assertEquals("Hello from Spring",message);
-	}
-	
-	@Test
-	public void shouldSaveTaskAndResetTaskInstanceOnBean() {		
-		Task oldTask = taskHome.getTask();
-		taskHome.getTask().setDescription("Sample Description");
-		taskHome.saveTask();
-		Assert.assertNotNull("Saved task ID is null,probably not saved",oldTask.getId());
-		Assert.assertNull("Task has not been reset",taskHome.getTask().getDescription());
-		Assert.assertNull("Task has not been reset",taskHome.getTask().getId());
-		Assert.assertNotSame("Task object has not been replaced",oldTask, taskHome.getTask());
-	}
+
+    @Autowired
+    private TaskHome taskHome;
+
+    @Test
+    public void shouldGetCorrectMessage() {
+        String message = taskHome.getMessage();
+        Assert.assertEquals("Hello from Spring", message);
+    }
+
+    @Test
+    public void shouldSaveTaskAndResetTaskInstanceOnBean() {
+        Task oldTask = taskHome.getTask();
+        taskHome.getTask().setDescription("Sample Description");
+        taskHome.saveTask();
+        Assert.assertNotNull("Saved task ID is null,probably not saved", oldTask.getId());
+        Assert.assertNull("Task has not been reset", taskHome.getTask().getDescription());
+        Assert.assertNull("Task has not been reset", taskHome.getTask().getId());
+        Assert.assertNotSame("Task object has not been replaced", oldTask, taskHome.getTask());
+    }
 }
