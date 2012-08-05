854a774956453cca859b864dde78145f72ee1b3e
qipengsun <qipeng.sun@gmail.com>
2012-08-06 00:26:44 +0800

    clear

---
+++ jsfweb/src/main/java/edu/sun/web/bean/TaskDao.java
@@ -0,0 +1,28 @@
+package edu.sun.web.bean;
+
+import edu.sun.web.model.Task;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class TaskDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Task task) {
+        entityManager.persist(task);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Task> list() {
+        return entityManager.createQuery("select t from Task t")
+                .getResultList();
+    }
+
+}
