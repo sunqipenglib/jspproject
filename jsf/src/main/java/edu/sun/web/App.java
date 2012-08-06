77632f9bec405c9e0718b31c8f2ec5594d3e55a1
qipengsun <qipeng.sun@gmail.com>
2012-08-06 22:59:16 +0800

    we init the jpa

--- jsf/src/main/java/edu/sun/web/App.java
+++ jsf/src/main/java/edu/sun/web/App.java
@@ -1,17 +1,32 @@
 package edu.sun.web;
 
 import edu.sun.entity.Hello;
+import org.jboss.logging.Logger;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 /**
  * Hello world!
  */
 public class App {
+
+    //static Logger logger = Logger.getLogger(App.class);
+
+    static Logger logger = Logger.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
         EntityManager entityManager = Persistence.createEntityManagerFactory("dbs").createEntityManager();
-        entityManager.persist(new Hello());
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        for (int i = 0; i < 1000; i++)
+            entityManager.persist(new Hello());
+        entityTransaction.commit();
+
+        logger.info(entityManager.createQuery("select u from Hello  u").getResultList().size());
     }
 }
